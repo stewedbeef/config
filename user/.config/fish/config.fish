0313d40c9ba921c652f4e722bbd0b6966e146294
stewedbeef <114018527+stewedbeef@users.noreply.github.com>
2026-02-09 06:09:52 -0800

    Add shell config files

--- user/.config/fish/config.fish
+++ user/.config/fish/config.fish
@@ -1,9 +1,9 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 	function fish_greeting
-		set --local full_name "$(getent passwd $USER | cut -d : -f5 | cut -d , -f1)"
+		set --local full_name "$(getent passwd $USER | cut -d : -f 5 | cut -d , -f 1)"
 		set --local raw_time "$(last -n 2 $USER --fulltimes | sed -n '2p' | awk '{print $7, $6, $9, $8}')"
-		set --local time_last_login "$(date -d "$raw_time" +'%a %d %b %Y, %-I:%M %p %Z')"
+		set --local time_last_login "$(date -d "$raw_time" +'%a %d %b %Y, %-I:%M:%S %p %Z')"
 		set --local time_now "$(date +'%a %d %b %Y, %-I:%M:%S %p %Z')"
 
 		echo "Welcome back, $full_name!"
